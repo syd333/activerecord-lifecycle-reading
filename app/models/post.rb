2321b8bc0c085dfe2beac3ce44d02e894c8eabc2
syd333 <sydmiranda@icloud.com>
2021-02-04 18:51:25 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  # New Code!!
+  before_save :email_author_about_post
 
   private
 
@@ -11,7 +16,12 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+
   def make_title_case
     self.title = self.title.titlecase
   end
-end
+end 
